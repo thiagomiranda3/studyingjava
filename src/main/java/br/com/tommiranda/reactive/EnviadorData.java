24cc0160481df48171379efeb1d7bad3c6566633
Thiago Miranda <marine.rlivielor@gmail.com>
2019-12-29 21:35:51 +0000

    estudando rxJava

---
+++ src/main/java/br/com/tommiranda/reactive/EnviadorData.java
@@ -0,0 +1,29 @@
+package br.com.tommiranda.reactive;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.stream.Stream;
+
+public class EnviadorData {
+
+    public static Observable<LocalDateTime> getTempos(Stream<LocalDate> datas) {
+        return Observable.create(emitter -> enviaDatas(emitter, datas));
+    }
+
+    private static void enviaDatas(ObservableEmitter<LocalDateTime> emitter, Stream<LocalDate> datas) {
+        datas.forEach(data -> emitter.onNext(LocalDateTime.of(data, LocalTime.now())));
+    }
+
+    public static void main(String[] args) {
+        var dataStream = Stream.iterate(LocalDate.now(), data -> data.plusDays(1));
+
+        Observable<LocalDateTime> observable = getTempos(dataStream);
+
+        // Me inscrevo para receber os dados que chegam do emitter
+        observable.subscribe(System.out::println);
+    }
+}
